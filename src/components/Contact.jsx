ba97fb7edfe44a76a9882eebec22a182e400f0a4
Ed Rojas <edurojas1212@gmail.com>
2024-03-19 11:17:01 -0600

    hidden html

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,36 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import githubIcon from "../Images/github-icon.png";
 import linkedIcon from "../Images/linkedin-icon.png";
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: ""
-  });
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Form submission logic using Fetch API
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...formData })
-    })
-      .then(() => alert("Success!"))
-      .catch((error) => alert(error));
-  };
-
-  const encode = (data) => {
-    return Object.keys(data)
-      .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-  };
+  // };
 
   return (
     <div className="contact-div-container">
@@ -42,11 +18,9 @@ const Contact = () => {
           name="contact"
           method="POST"
           data-netlify="true"
-          onSubmit={handleSubmit}
           data-netlify-honeypot="bot-field"
+          netlify
         >
-          {/* Hidden form name field */}
-          <input type="hidden" name="form-name" value="contact" />
 
           <div className="input-container inline">
             <div className="input-container">
@@ -59,8 +33,7 @@ const Contact = () => {
                 className="contact-input"
                 name="name"
                 placeholder="name"
-                value={formData.name}
-                onChange={handleChange}
+                value="contact"
                 required
               />
             </div>
@@ -75,8 +48,6 @@ const Contact = () => {
                 className="contact-input"
                 placeholder="email"
                 name="email"
-                value={formData.email}
-                onChange={handleChange}
                 required
               />
             </div>
@@ -93,8 +64,6 @@ const Contact = () => {
               placeholder="message"
               name="message"
               rows="4"
-              value={formData.message}
-              onChange={handleChange}
               required
             />
           </div>
