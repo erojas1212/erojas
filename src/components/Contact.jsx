cff210b627722e4befcb0624e89b7d4856690a56
Ed Rojas <edurojas1212@gmail.com>
2024-03-28 15:17:57 -0600

    added send email button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,47 +1,37 @@
 // import React, { useState } from "react";
 // import githubIcon from "../Images/github-icon.png";
 // import linkedIcon from "../Images/linkedin-icon.png";
-
 // const Contact = () => {
-
-//   const [name, setName] = useState("")
-//   const [email, setEmail] = useState("")
-//   const [messsage, setMesssage] = useState("")
-
-//   const handleSubmit = async(event) => {
-//     event.preventDefault();
-//     try {
-//       const response = await fetch("/", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({ name, email, messsage })
-//       })
-//       if(response.ok){
-//         alert("Success!")
-//       }
-//     } catch (error) {
-//       // alert(error)
-//       console.log(error)
-//     }
-
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [message, setMessage] = useState("");
+//   const handleSubmit = async (event) => {
+//           event.preventDefault();
+//           try {
+//             const response = await fetch("/", {
+//               method: "POST",
+//               headers: { "Content-Type": "application/json" },
+//               body: JSON.stringify({ name, email, message })
+//             })
+//             if(response.ok){
+//               alert("Success!")
+//             }
+//           } catch (error) {
+//             console.log(error)
+//           }
 //   };
-
 //   return (
 //     <div className="contact-div-container">
 //       <div className="contact-container">
 //         <h2 className="contact-title">Contact</h2>
-
 //         <form
 //           className="contact-form"
 //           name="contact"
-//           // method="post"
-//           // data-netlify="true"
-//           netlify
-//           // data-netlify-honeypot="bot-field"
+//           method="POST"
+//           data-netlify="true"
 //           onSubmit={handleSubmit}
 //         >
-
-
+//           <input type="hidden" name="form-name" value="contact" />
 //           <div className="input-container inline">
 //             <div className="input-container">
 //               <label htmlFor="name" className="contact-label">
@@ -58,7 +48,6 @@
 //                 onChange={(event) => setName(event.target.value)}
 //               />
 //             </div>
-
 //             <div className="input-container">
 //               <label htmlFor="email" className="contact-label">
 //                 Email:
@@ -75,12 +64,10 @@
 //               />
 //             </div>
 //           </div>
-
 //           <div className="input-container">
 //             <label htmlFor="message" className="contact-label">
 //               Message:
 //             </label>
-
 //             <textarea
 //               id="message"
 //               className="contact-textarea"
@@ -88,134 +75,38 @@
 //               name="message"
 //               rows="4"
 //               required
-//               value={messsage}
-//               onChange={(event) => setMesssage(event.target.value)}
+//               value={message}
+//               onChange={(event) => setMessage(event.target.value)}
 //             />
 //           </div>
-
 //           <button type="submit" className="contact-button">
 //             Submit
 //           </button>
 //         </form>
-
-//         <div className="social-media-buttons">
-//           <ul className="social">
-//             <li>
-//               <a
-//                 href="https://www.linkedin.com/in/ed-rojas/"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <img
-//                   src={linkedIcon}
-//                   alt="LinkedIn"
-//                   className="linkedin-icon"
-//                 />
-//               </a>
-//             </li>
-//             <li>
-//               <a
-//                 href="https://github.com/erojas1212/VSchool"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <img src={githubIcon} alt="Github" className="github-icon" />
-//               </a>
-//             </li>
-//           </ul>
 //         </div>
 //       </div>
 //     </div>
 //   );
 // };
-
 // export default Contact;
-import React, { useState } from "react";
+
+import React from "react";
 import githubIcon from "../Images/github-icon.png";
 import linkedIcon from "../Images/linkedin-icon.png";
+
 const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const handleSubmit = async (event) => {
-          event.preventDefault();
-          try {
-            const response = await fetch("/", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ name, email, message })
-            })
-            if(response.ok){
-              alert("Success!")
-            }
-          } catch (error) {
-            // alert(error)
-            console.log(error)
-          }
-  };
+  const email = "edurojas1212@gmail.com";
+
   return (
     <div className="contact-div-container">
       <div className="contact-container">
         <h2 className="contact-title">Contact</h2>
-        <form
-          className="contact-form"
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          onSubmit={handleSubmit}
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <div className="input-container inline">
-            <div className="input-container">
-              <label htmlFor="name" className="contact-label">
-                Name:
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="contact-input"
-                name="name"
-                placeholder="name"
-                required
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-              />
-            </div>
-            <div className="input-container">
-              <label htmlFor="email" className="contact-label">
-                Email:
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="contact-input"
-                placeholder="email"
-                name="email"
-                required
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-              />
-            </div>
-          </div>
-          <div className="input-container">
-            <label htmlFor="message" className="contact-label">
-              Message:
-            </label>
-            <textarea
-              id="message"
-              className="contact-textarea"
-              placeholder="message"
-              name="message"
-              rows="4"
-              required
-              value={message}
-              onChange={(event) => setMessage(event.target.value)}
-            />
-          </div>
-          <button type="submit" className="contact-button">
-            Submit
+        <div className="contact-form">
+          <p className="email-text">If you prefer, you can also contact me via email:</p>
+          <button className="contact-button">
+            <a href={`mailto:${email}`} className="email-link">Send Email</a>
           </button>
-        </form>
+        </div>
         <div className="social-media-buttons">
           <ul className="social">
             <li>
@@ -233,7 +124,7 @@ const Contact = () => {
             </li>
             <li>
               <a
-                href="https://github.com/erojas1212/VSchool"
+                href="https://github.com/erojas1212"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -246,4 +137,5 @@ const Contact = () => {
     </div>
   );
 };
+
 export default Contact;
